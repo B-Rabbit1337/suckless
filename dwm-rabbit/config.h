3fae652bda1790e56297efa7466c50db8f9734b8
Györkös Imre <88290711+brabbit1337@users.noreply.github.com>
2023-11-14 23:08:09 +0100

    Update config.h

--- dwm-rabbit/config.h
+++ dwm-rabbit/config.h
@@ -51,8 +51,6 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "steam",   NULL,     NULL,           1 << 8,    0,          0,           0,        -1 },
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "kitty",   NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
