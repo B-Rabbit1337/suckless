aa6b054887491e79d5553cabf7810d5fdda12460
Györkös Imre <88290711+brabbit1337@users.noreply.github.com>
2023-11-16 17:51:40 +0100

    Update config.h

--- dwm-rabbit/config.h
+++ dwm-rabbit/config.h
@@ -31,15 +31,17 @@ static const char *const autostart[] = {
   "flameshot", NULL,
   "lxpolkit", NULL,
   "dunst", NULL,
-	"picom", NULL,
-  "feh", "--bg-fill", "/usr/share/backgrounds/nordic-wallpapers/ign-0011.png", NULL,
+  "conky", NULL,
+  "kdeconnect-cli", NULL,
+  "picom", NULL,
+  "feh", "--bg-fill", "/usr/share/backgrounds/archlinux/simple.png", NULL,
   "xrandr", "--output", "HDMI-0", "--primary", "mode", "2560x1080", "--rate", "60", NULL,
   "slstatus", NULL,
 	NULL /* terminate */
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5" };
 
 static const char ptagf[] = "[%s %s]";	/* format of a tag label */
 static const char etagf[] = "[%s]";	/* format of an empty tag */
@@ -51,6 +53,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "steam",   NULL,     NULL,           1 << 8,    0,          0,           0,        -1 },
+	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "kitty",   NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
@@ -121,10 +125,6 @@ static Key keys[] = {
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
 	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ MODKEY|ControlMask|ShiftMask, XK_r,      spawn,          SHCMD("systemctl reboot")},
 	{ MODKEY|ControlMask|ShiftMask, XK_s,      spawn,          SHCMD("systemctl shutdown now")},
